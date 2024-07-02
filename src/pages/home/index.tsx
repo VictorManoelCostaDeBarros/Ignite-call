f3b1df4a8bcb1e4ba19547066256de80baf677e4
VictorManoelCostaDeBarros <victor.manoel8@hotmail.com>
2024-07-02 16:14:30 -0300

    first commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,39 @@
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+import { Container, Hero, Preview } from './styles'
+
+import previewImage from '../../assets/app-preview.png'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+import { NextSeo } from 'next-seo'
+
+export default function Home() {
+  return (
+    <>
+      <NextSeo
+        title="Uncomplicate your schedule | Ignite Call"
+        description="Connect your calendar and let people schedule appointments in their free time."
+      />
+      <Container>
+        <Hero>
+          <Heading size="4xl">Agendamento descomplicado</Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marque agendamentos
+            no seu tempo livre.
+          </Text>
+
+          <ClaimUsernameForm />
+        </Hero>
+
+        <Preview>
+          <Image
+            src={previewImage}
+            height={400}
+            quality={100}
+            priority
+            alt="Calendário simbolizando aplicação em funcionamento"
+          />
+        </Preview>
+      </Container>
+    </>
+  )
+}
